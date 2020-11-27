4a695bcfa84708c89c8eb86e052d75f48336ac43
Cossette Alexis <gabuyalexis@gmail.com>
2020-11-27 11:53:25 +0800

    Updated CV Link

--- js/jquery.chocolat.js
+++ js/jquery.chocolat.js
@@ -4,6 +4,26 @@
 // });
 
 
+// var i = 0;
+// if ($(".skills").is(":visible")) {
+// if (i == 0) {
+// 	i = 1;
+// 	var elem = document.getElementsByClassName("progress-bar");
+// 	var width = 1;
+// 	var id = setInterval(frame, 10);
+// 	function frame() {
+// 		if (width >= 100) {
+// 			clearInterval(id);
+// 			i = 0;
+// 		} else {
+// 			width++;
+// 			elem.style.width = width + "%";
+// 		}
+// 	}
+// }
+// }
+
+
 (function ($) {
 	images = [];
 	var calls = 0;
@@ -350,3 +370,5 @@
 
 	};
 })(jQuery);
+
+
